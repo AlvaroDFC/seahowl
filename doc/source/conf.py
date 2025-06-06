c72885567788a67534e5d614ba04df518ab105ae
Tristan de Lataillade <delataillade.tristan@gmail.com>
2025-06-06 13:52:37 +0200

    Release v0.9.3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ copyright = "2022-2025, TotalEnergies-SE"
 author = "Power R&D Team"
 
 # The full version, including alpha/beta/rc tags
-release = "0.9.2"
+release = "0.9.3"
 
 
 # -- General configuration ---------------------------------------------------
