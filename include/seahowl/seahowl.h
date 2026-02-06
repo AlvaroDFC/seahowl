57623fa0442c99a7de623df77a7b99df7ab80e51
Tristan de Lataillade <tristan.de-lataillade@totalenergies.com>
2026-02-06 02:20:41 +0100

    Add seahowl.h umbrella header

---
+++ include/seahowl/seahowl.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "seahowl/commons.h"
+#include "seahowl/core.h"
+#include "seahowl/elasto.h"
+#include "seahowl/env.h"
+#include "seahowl/fluid.h"
+#include "seahowl/io.h"
+#include "seahowl/servo.h"
